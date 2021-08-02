59ccffaf757b889ffbec413f9ad78fcfda2f4cbd
Dhenyson Jhean <dhenysonjhean@gmail.com>
2021-08-02 20:08:31 -0300

    Movimentação do personagem

--- src/App.js
+++ src/App.js
@@ -41,10 +41,11 @@ function App() {
         this.platforms.create(50, 250, 'ground')
         this.platforms.create(750, 220, 'ground')
 
-        let player = this.physics.add.sprite(100, 450, 'dude')
+        this.player = this.physics.add.sprite(100, 450, 'dude')
+        this.player.setBounce(0.2)
+        this.player.setCollideWorldBounds(true)
 
-        player.setBounce(0.2)
-        player.setCollideWorldBounds(true)
+        this.physics.add.collider(this.player, this.platforms)
 
         this.anims.create({
             key: 'left',
@@ -73,7 +74,27 @@ function App() {
         })
     }
 
-    function update() {}
+    function update() {
+        this.cursors = this.input.keyboard.createCursorKeys()
+
+        if (this.cursors.left.isDown) {
+            this.player.setVelocityX(-160)
+
+            this.player.anims.play('left', true)
+        } else if (this.cursors.right.isDown) {
+            this.player.setVelocityX(160)
+
+            this.player.anims.play('right', true)
+        } else {
+            this.player.setVelocityX(0)
+
+            this.player.anims.play('turn')
+        }
+
+        if (this.cursors.up.isDown && this.player.body.touching.down) {
+            this.player.setVelocityY(-330)
+        }
+    }
 
     return (
         <div
