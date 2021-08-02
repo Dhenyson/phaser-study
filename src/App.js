567737cfe4ad9c35e9d50f9fbb28c5d9f43497ef
Dhenyson Jhean <dhenysonjhean@gmail.com>
2021-08-02 19:50:18 -0300

    Add player

--- src/App.js
+++ src/App.js
@@ -10,7 +10,10 @@ function App() {
         type: Phaser.AUTO,
         width: 800,
         height: 600,
-        physics: { default: 'arcade', arcade: { gravity: { y: 1000 } } },
+        physics: {
+            default: 'arcade',
+            arcade: { gravity: { y: 300 }, debug: false }
+        },
         scene: { preload, create, update }
     })
 
@@ -37,6 +40,37 @@ function App() {
         this.platforms.create(600, 400, 'ground')
         this.platforms.create(50, 250, 'ground')
         this.platforms.create(750, 220, 'ground')
+
+        let player = this.physics.add.sprite(100, 450, 'dude')
+
+        player.setBounce(0.2)
+        player.setCollideWorldBounds(true)
+
+        this.anims.create({
+            key: 'left',
+            frames: this.anims.generateFrameNumbers('dude', {
+                start: 0,
+                end: 3
+            }),
+            frameRate: 10,
+            repeat: -1
+        })
+
+        this.anims.create({
+            key: 'turn',
+            frames: [{ key: 'dude', frame: 4 }],
+            frameRate: 20
+        })
+
+        this.anims.create({
+            key: 'right',
+            frames: this.anims.generateFrameNumbers('dude', {
+                start: 5,
+                end: 8
+            }),
+            frameRate: 10,
+            repeat: -1
+        })
     }
 
     function update() {}
