82df434136046f40d3f37331c51b4c8e8901c5b6
Dhenyson Jhean <dhenysonjhean@gmail.com>
2021-08-02 21:03:39 -0300

    Jogo finalizado

--- src/App.js
+++ src/App.js
@@ -41,27 +41,33 @@ function App() {
         this.platforms.create(50, 250, 'ground')
         this.platforms.create(750, 220, 'ground')
 
-        this.player = this.physics.add.sprite(100, 450, 'dude')
+        this.player = this.physics.add.sprite(20, 450, 'dude')
         this.player.setBounce(0.2)
         this.player.setCollideWorldBounds(true)
 
         this.physics.add.collider(this.player, this.platforms)
 
-        this.anims.create({
-            key: 'left',
-            frames: this.anims.generateFrameNumbers('dude', {
-                start: 0,
-                end: 3
-            }),
-            frameRate: 10,
-            repeat: -1
-        })
+        this.anims.create(
+            {
+                key: 'left',
+                frames: this.anims.generateFrameNumbers('dude', {
+                    start: 0,
+                    end: 3
+                }),
+                frameRate: 10,
+                repeat: -1
+            },
+            console.log('andou')
+        )
 
-        this.anims.create({
-            key: 'turn',
-            frames: [{ key: 'dude', frame: 4 }],
-            frameRate: 20
-        })
+        this.anims.create(
+            {
+                key: 'turn',
+                frames: [{ key: 'dude', frame: 4 }],
+                frameRate: 20
+            },
+            console.log('andou')
+        )
 
         this.anims.create({
             key: 'right',
@@ -83,10 +89,6 @@ function App() {
             child.setBounceY(Phaser.Math.FloatBetween(0.4, 0.8))
         })
 
-        function collectStar(player, star) {
-            star.disableBody(true, true)
-        }
-
         this.physics.add.collider(this.stars, this.platforms)
         this.physics.add.overlap(
             this.player,
@@ -97,7 +99,69 @@ function App() {
         )
 
         let score = 0
+        let maxScore = 0
         let scoreText
+        scoreText = this.add.text(16, 16, 'Score: 0', {
+            fontSize: '32px',
+            fill: '#000'
+        })
+        let maxScoreText
+        maxScoreText = this.add.text(16, 50, 'Max Score: 0', {
+            fontSize: '32px',
+            fill: '#000'
+        })
+
+        this.bombs = this.physics.add.group()
+
+        this.physics.add.collider(this.bombs, this.platforms)
+
+        this.physics.add.collider(this.player, this.bombs, hitBomb, null, this)
+
+        function collectStar(player, star) {
+            star.disableBody(true, true)
+
+            score += 10
+
+            scoreText.setText('Score: ' + score)
+            if (score >= maxScore) {
+                maxScoreText.setText('Max Score: ' + score)
+            }
+
+            if (this.stars.countActive(true) === 0) {
+                this.stars.children.iterate(function (child) {
+                    child.enableBody(true, child.x, 0, true, true)
+                })
+
+                var x =
+                    player.x < 400
+                        ? Phaser.Math.Between(400, 800)
+                        : Phaser.Math.Between(0, 400)
+
+                var bomb = this.bombs.create(x, 16, 'bomb')
+                bomb.setBounce(1)
+                bomb.setCollideWorldBounds(true)
+                bomb.setVelocity(Phaser.Math.Between(-200, 200), 20)
+            }
+        }
+
+        function hitBomb(player, bomb) {
+            this.physics.pause()
+
+            player.setTint(0xff0000)
+
+            player.anims.play('turn')
+            // this.stars
+
+            this.gameOver = true
+        }
+        function collectStar2(player, bomb) {
+            this.physics.pause()
+
+            player.anims.play('turn')
+            player(10, 10)
+
+            this.gameOver = true
+        }
     }
 
     function update() {
